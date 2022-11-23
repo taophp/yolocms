021bbf21240ea21c43f55fd1c727163ea82614e2
Stéphane Mourey <s+github@stephanemourey.fr>
2022-11-23 23:09:11 +0100

    Add custom error handler

--- src/admin/public/index.php
+++ src/admin/public/index.php
@@ -1,4 +1,12 @@
 <?php
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+    
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
 
 use App\Kernel;
 
