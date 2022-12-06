5c697f399eadb0bb11d7133fc02220c70935692c
Stéphane Mourey <s+github@stephanemourey.fr>
2022-12-06 23:16:44 +0100

    Work in progress

--- src/admin/src/Controller/IndexController.php
+++ src/admin/src/Controller/IndexController.php
@@ -21,27 +21,14 @@ class IndexController extends AbstractController
             throw new Exception("File not found or access refused: ".$page);
         }
 
-        $srcContents = file_get_contents($srcFullFileName);
+        [$head, $body] = $this->parse($srcFullFileName);
 
-        try {
-            [$head, $body] = explode("\n---\n", $srcContents, 2);
-        } catch (\ErrorException $e) {
-            if ($e->getMessage()==='Warning: Undefined array key 1') {
-                throw new \ErrorException('Invalid source file format. File: '.$page);
-            }else{
-                throw $e;
-            }
-        }
-
-        $itemConfig = Yaml::parse($head);
-
-        $converter = new CommonMarkConverter();
         /**
          * @todo make the default theme web configurable
          */
         return $this->render('index.html.twig', [
-            'title' => $itemConfig['title'],
-            'body'  => $converter->convert($body),
+            'title' => $head->title,
+            'body'  => $body,
             'adminbar'   => $this->getAdminBar(),
         ]);
     }
@@ -49,4 +36,88 @@ class IndexController extends AbstractController
     protected function getAdminBar() : String {
         return $this->renderView('@admin/adminbar.html.twig');
     }
+
+    #[Route('/_build', name: '_build', priority: 1)]
+    public function build() : Response {
+        $body = '';
+
+        $dstDir = realpath(__DIR__ . '/../../build');
+
+        // Copying assets
+        $assetsDir = realpath(__DIR__ . '/../../themes/default/assets');
+        static::rcopy($assetsDir, $dstDir . '/assets');
+
+        // Build contents
+        $srcDir =  realpath(__DIR__.'/../../contents');
+        $srcFiles = glob($srcDir . '/*', GLOB_MARK);
+
+        foreach ($srcFiles as $srcFile) {
+            dump($srcFile);
+            if (in_array(substr(basename($srcFile),0,1),['_','.'])) { // ignore
+                dump('Continue:' . __LINE__);
+                continue;
+            }
+            $dstFile = str_replace($srcDir, $dstDir, $srcFile);
+            if (is_dir($srcFile)) { // make directory if not exists
+                if (!file_exists($dstFile)) {
+                    mkdir($dstFile, '0777', true);
+                }
+                dump('Continue:' . __LINE__);
+                continue;
+            }
+            if (substr($srcFile,-3)==='.md') { // parse
+                [$head, $body] = $this->parse($srcFile);
+                $dstFile = substr($dstFile, 0, strrpos($dstFile, '.md')) . '.' . (empty($head->layout) ? 'html' : pathinfo($head->layout, PATHINFO_EXTENSION));
+                dump('Parse', $dstFile);
+                file_put_contents($dstFile, $this->renderView(empty($head->layout) ? 'index.html.twig' : $head->layout, [
+                    'title' => $head->title,
+                    'body' => $body,
+                ]));
+                continue;
+            }
+            // copy
+            dump('Copy', $srcFile, $dstFile);
+            copy($srcFile, $dstFile);
+        }
+
+        return $this->render('index.html.twig', [
+            'title' => 'Build',
+            'body'  => $body,
+            'adminbar'   => $this->getAdminBar(),
+        ]);
+    }
+ 
+    public function parse(string $filename) :  array {
+        $srcContents = file_get_contents($filename);
+
+        try {
+            [$head, $body] = explode("\n---\n", $srcContents, 2);
+        } catch (\ErrorException $e) {
+            if ($e->getMessage()==='Warning: Undefined array key 1') {
+                throw new \ErrorException('Invalid source file format. File: '.$filename);
+            }else{
+                throw $e;
+            }
+        }
+
+        $converter = new CommonMarkConverter();
+        return [Yaml::parse($head,Yaml::PARSE_OBJECT_FOR_MAP), $converter->convert($body)];
+        
+    }
+
+    public static function rcopy(string $src, string $dst) {
+        if (is_dir($src)) {
+            if (!file_exists($dst)) {
+                mkdir($dst);
+            }
+            $files = scandir($src);
+            foreach ($files as $file)
+            if ($file != "." && $file != "..") static::rcopy("$src/$file", "$dst/$file");
+        } else if (file_exists($src)) {
+            if (file_exists($dst) && filemtime($dst) >= filemtime($src)) {
+                return;
+            }
+            copy($src, $dst);
+        }
+      }
 }
